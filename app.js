81d114721edebb5899ed3ec14b61ce095e973f70
ywettai-dev <ywettai.dev@gmail.com>
2019-05-25 11:40:28 +0630

    Add static folder: public with express

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const request = require('request');
 
+//Express static, public
+app.use(express.static('public/'));
+
 //Express server
 app.listen(port, () => console.log(`News-letter signup starts on ${port}`));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Signup Home
+app.get('/', (req, res) => res.sendFile(__dirname + `/signup.html`));
+
+console.log(__dirname + `/signup.html`);
